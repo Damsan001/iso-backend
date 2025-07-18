a70ba4629b5db5040c44a1e356ba48c52dd3f1f4
German <hernandezcortesgerman@gmail.com>
2025-07-18 16:18:45 -0600

    feat: add clear_name utility function for sanitizing document names

---
+++ app/utils/text_utils.py
@@ -0,0 +1,5 @@
+def clear_name(name: str) -> str:
+    """
+    Limpia el nombre del documento eliminando caracteres especiales y espacios.
+    """
+    return ''.join(e for e in name if e.isalnum() or e in (' ', '-', '_')).strip()
